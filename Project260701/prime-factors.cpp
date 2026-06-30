1a01907c3fa0d1268f843adc50d71c537061749d
mgKang <kangtop729@gmail.com>
2026-07-01 08:53:46 +0900

    Green #2

--- Project260701/prime-factors.cpp
+++ Project260701/prime-factors.cpp
@@ -1,10 +1,16 @@
 #include <vector>
-
+#include <iostream>
 using std::vector;
-
+using std::cout;
 class PrimeFactors {
 public:
 	vector<int> of(int i) {
-		return {};
+		vector<int> result = {};
+		for (int n = 2; n <= i/2 + 1; n++) {
+			if (i % n == 0) {
+				result.push_back(n);
+			}
+		}
+		return result;
 	}
 };
