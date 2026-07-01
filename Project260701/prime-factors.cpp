5fefa6395fea15b25ef8aa539f95e6a28d0b650e
mgKang <kangtop729@gmail.com>
2026-07-01 09:03:28 +0900

    Green FINAL

--- Project260701/prime-factors.cpp
+++ Project260701/prime-factors.cpp
@@ -4,11 +4,16 @@ using std::vector;
 using std::cout;
 class PrimeFactors {
 public:
-	vector<int> of(int i) {
+	vector<int> of(int number) {
 		vector<int> result = {};
-		for (int n = 2; n <= i/2 + 1; n++) {
-			if (i % n == 0) {
-				result.push_back(n);
+		int divisor = 2;
+		while(number != 1){
+			if (number % divisor == 0) {
+				result.push_back(divisor);
+				number /= divisor;
+			}
+			else {
+				divisor++;
 			}
 		}
 		return result;
--- Project260701/prime-factors_test.cpp
+++ Project260701/prime-factors_test.cpp
@@ -1,13 +1,37 @@
 #include "gmock/gmock.h"
 #include "prime-factors.cpp"
 
-TEST(PrimeFactors, Of1) {
+using namespace testing;
+class PrimeFixture : public Test {
+public:
 	PrimeFactors prime_factor;
-	vector<int> expected = {};
+	vector<int> expected;
+};
+TEST_F(PrimeFixture, Of1) {
+	expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
-TEST(PrimeFactors, Of2) {
-	PrimeFactors prime_factor;
-	vector<int> expected = {2};
+TEST_F(PrimeFixture, Of2) {
+	expected = {2};
 	EXPECT_EQ(expected, prime_factor.of(2));
+}
+TEST_F(PrimeFixture, Of3) {
+	expected = {3};
+	EXPECT_EQ(expected, prime_factor.of(3));
+}
+TEST_F(PrimeFixture, Of4) {
+	expected = {2, 2};
+	EXPECT_EQ(expected, prime_factor.of(4));
+}
+TEST_F(PrimeFixture, Of6) {
+	expected = {2, 3};
+	EXPECT_EQ(expected, prime_factor.of(6));
+}
+TEST_F(PrimeFixture, Of9) {
+	expected = {3, 3};
+	EXPECT_EQ(expected, prime_factor.of(9));
+}
+TEST_F(PrimeFixture, Of12) {
+	expected = {2, 2, 3};
+	EXPECT_EQ(expected, prime_factor.of(12));
 }
